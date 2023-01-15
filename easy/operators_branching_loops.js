dff462f4631658d454a9b18267f68a97ebfab85b
yashpurkar <yashpurkar7079@gmail.com>
2023-01-15 13:47:40 +0530

    question_1

--- easy/operators_branching_loops.js
+++ easy/operators_branching_loops.js
@@ -1,4 +1,18 @@
-// Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+// 1️⃣ Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+let num1 = 5;
+let num2 = 13;
+let num3 = 7;
+let num4 = 21;
+let num5 = 48;
+
+function addNmbers(...numbers) {
+  let sum = 0;
+  numbers.forEach((number) => {
+    sum = sum + number;
+  })
+  return sum;
+}
+console.log(addNmbers(num1, num2, num3, num4, num5));
 
 // Write a program to take a number input from user and determine whether the number is odd or even.
 
