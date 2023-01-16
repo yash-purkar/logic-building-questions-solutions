e2a95b34c985c193ea6908f5621ad5866eeeeddb
yashpurkar <yashpurkar7079@gmail.com>
2023-01-17 00:07:33 +0530

    question_2_and_3

--- intermediate/operators_branching_loops.js
+++ intermediate/operators_branching_loops.js
@@ -1,8 +1,8 @@
 // Intermediate
 
-// Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz".
+// 1️⃣ Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz".
 
-let numbers = [];
+/*let numbers = [];
 for (let i = 1; i <= 100; i++) {
   numbers.push(i);
   if (i % 3 === 0 && i % 5 === 0) {
@@ -16,21 +16,41 @@ for (let i = 1; i <= 100; i++) {
   }
 }
 
-console.log(numbers);
-
-
-
+console.log(numbers);*/
 // Your output should look something like this 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17 .....
 
-// Print the following star pattern :-
+
+// 2️⃣ Print the following star pattern :-
 
 // *
 // * *
 // * * *
 // * * * *
 // * * * * *
+/*for (let i = 0; i < 5; i++) { // rows 
+  for (let j = 0; j <= i; j++) { //columns
+    document.write("*");
+  }
+  document.write("<br/>");
+}*/
 
-// Write a program to take a number input from user and print multiplication table 12 times for that number.
+
+// 3️⃣ Write a program to take a number input from user and print multiplication table 12 times for that number.
+
+const multiplication = (n) => {
+  for (let i = 1; i <= 12; i++) {
+    for (let j = 1; j <= 10; j++) {
+      console.log(n * j);
+    }
+    console.log("\n");
+  }
+}
+multiplication(5);
+
+
+
+
+
 
 // Write a program to return a Fibonacci series : 0,1,1,2,3,5,8,13,21....
 
