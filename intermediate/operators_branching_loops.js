907b3aeee167edbc82b642f987540383e013865a
yashpurkar <yashpurkar7079@gmail.com>
2023-01-17 20:01:04 +0530

    e.g1 solution2

--- intermediate/operators_branching_loops.js
+++ intermediate/operators_branching_loops.js
@@ -16,6 +16,26 @@ for (let i = 1; i <= 100; i++) {
   }
 }
 
+// solution2
+let numbers=[];
+for(let i=1; i<=100; i++) {
+  
+  if(i%3 ==0 && i%5 ==0) {
+      numbers.push("FizzBuzz")
+  }
+  else if(i%3 ==0 ){
+      numbers.push("Fizz")
+  }
+  else if(i%5==0) {
+      numbers.push("Buzz")
+  }
+  else{
+      numbers.push(i);
+  }
+}
+
+console.log(numbers);
+
 console.log(numbers);*/
 // Your output should look something like this 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17 .....
 
@@ -27,7 +47,7 @@ console.log(numbers);*/
 // * * *
 // * * * *
 // * * * * *
-/*for (let i = 0; i < 5; i++) { // rows 
+/*for (let i = 0; i < 5; i++) { // rows
   for (let j = 0; j <= i; j++) { //columns
     document.write("*");
   }
@@ -37,7 +57,7 @@ console.log(numbers);*/
 
 // 3️⃣ Write a program to take a number input from user and print multiplication table 12 times for that number.
 
-const multiplication = (n) => {
+/*const multiplication = (n) => {
   for (let i = 1; i <= 12; i++) {
     for (let j = 1; j <= 10; j++) {
       console.log(n * j);
@@ -45,8 +65,7 @@ const multiplication = (n) => {
     console.log("\n");
   }
 }
-multiplication(5);
-
+multiplication(5);*/
 
 
 
@@ -59,3 +78,12 @@ multiplication(5);
 // Write a Program to take a number input from user and find if the number is Prime or not.
 
 // Write a program to take a day as an input and determine whether it is a weekday or weekend. Example: Tuesday is weekday.
+
+
+// Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz".
+
+let numbers = [];
+for (let i = 1; i <= 100; i++) {
+  numbers.push(i);
+}
+
