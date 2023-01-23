a30c4471e7f20b99d39eadd5f21e28ae286b0a5f
yashpurkar <yashpurkar7079@gmail.com>
2023-01-23 23:49:36 +0530

    upto intermediate Q3

---
+++ intermediate/functions.js
@@ -0,0 +1,70 @@
+// 1️⃣Given an array, your function should return the length of the array.
+// Example:
+// Input: arrayLength([1,5,3,7,8]) ––> Output: 5
+
+/*function lengthOfArr(arr) {
+  let ln = arr.length;
+  console.log(ln);
+}
+lengthOfArr([1, 5, 3, 7, 8]);*/
+
+
+// 2️⃣Given an array and an item, your function should return the index at which the item is present.
+// Example:
+// Input: indexOf([1,6,3,5,8,9], 3) ––> Output: 2
+
+/*function findIndex(arr, elem) {
+  let index = arr.indexOf(elem);
+  console.log(index);
+}
+findIndex([1, 6, 3, 5, 8, 9], 3);*/
+
+// 3️⃣ Given an array and two numbers, your function should replace all entries of first number in an array with the second number.
+// Example:
+// Input: replace([1,5,3,5,6,8], 5, 10) ––> Output: [1,10,3,10,6,8]
+
+
+function replaceElems(arr, num1, num2) {
+
+  let newArr = arr.map(function (elem) {
+    if (elem === num1) { //1===5 F | 5===f T | 3==5F | 5===5T |6===5F | 8===5F
+      return num2 // [1,10] [1,10,3,10]
+    }
+    else {
+      return elem  //[1] [1,10,3] [1,10,3,10,6], [1,10,3,10,6,8];
+    }
+  })
+  return newArr;
+  //The map() method will check the condition e.g elem===num1 i.e 5===5 True , then it will return the num2 i.e 10.  Map method returns the new array, It calls the callback function on every element of an array.
+
+
+  // With For Loop
+  /*   for (let i = 0; i < arr.length; i++) {
+       if (arr[i] === num1) {
+         arr[i] = num2;
+       }
+     }
+     return arr;*/
+
+}
+console.log(replaceElems([1, 5, 3, 5, 6, 8], 5, 10))
+
+
+
+
+
+
+
+
+
+// Given two arrays, your function should return single merged array.
+// Example:
+// Input: mergeArray([1,3,5], [2,4,6]) ––> Output: [1,3,5,2,4,6]
+
+// Given a string and an index, your function should return the character present at that index in the string.
+// Example:
+// Input: charAt("neoGcamp", 4) ––> Output: c
+
+// Given two dates, your function should return which one comes before the other.
+// Example:
+// Input: minDate('02/05/2021', '24/01/2021') ––> Output: 24/01/2021
